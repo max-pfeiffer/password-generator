3db9f54bc7ffb1cc1151db1f3073870a7bffc6bb
Max Pfeiffer <max@maxpfeiffer.ch>
2022-11-21 17:34:27 +0100

    Optimized api_error_handler Decorator
    I contained a not needed else statement.

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -20,12 +20,11 @@ def api_error_handler(function: Callable):
     :return:
     """
     # pylint: disable=duplicate-code
-    # pylint: disable=no-else-return
 
     if iscoroutinefunction(function):
 
         @wraps(function)
-        async def wrapper(*args, **kwargs):
+        async def async_wrapper(*args, **kwargs):
             try:
                 return await function(*args, **kwargs)
             except GeneratePasswordInvalidFlagsError as flags_exc:
@@ -47,30 +46,29 @@ def api_error_handler(function: Callable):
                     detail=detail,
                 ) from exc
 
-        return wrapper
-    else:
+        return async_wrapper
 
-        @wraps(function)
-        def wrapper(*args, **kwargs):
-            try:
-                return function(*args, **kwargs)
-            except GeneratePasswordInvalidFlagsError as flags_exc:
-                detail: dict = HTTPExceptionResponseModel.create_from_exception(
-                    flags_exc
-                ).dict()
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail=detail,
-                ) from flags_exc
-            except HTTPException as http_exc:
-                raise http_exc
-            except Exception as exc:
-                detail: dict = HTTPExceptionResponseModel.create_from_exception(
-                    exc
-                ).dict()
-                raise HTTPException(
-                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail=detail,
-                ) from exc
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except GeneratePasswordInvalidFlagsError as flags_exc:
+            detail: dict = HTTPExceptionResponseModel.create_from_exception(
+                flags_exc
+            ).dict()
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=detail,
+            ) from flags_exc
+        except HTTPException as http_exc:
+            raise http_exc
+        except Exception as exc:
+            detail: dict = HTTPExceptionResponseModel.create_from_exception(
+                exc
+            ).dict()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=detail,
+            ) from exc
 
-        return wrapper
+    return wrapper
